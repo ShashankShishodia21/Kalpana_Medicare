a40ed709f536b2dddbf1529f15e9194f7147d54a
Shashank Shishodia <shishodia.14shashank@gmail.com>
2024-08-07 12:51:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Kalpana_Medicare_Main/',
+  base:'/Kalpana_Medicare/',
   plugins: [react()],
 })
